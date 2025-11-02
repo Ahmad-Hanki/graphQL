2e82b4c227cbd4734fbae4acf5bfd7c30df91694
Ahmad <itxti909@gmail.com>
2025-11-02 17:47:18 +0300

    relational data. parents and args

--- index.js
+++ index.js
@@ -16,6 +16,51 @@ const resolvers = {
     reviews: () => {
       return reviews;
     },
+
+    game: (parents, args, context) => {
+      const { id } = args;
+      console.log("parent in Game ", parents); // undefined
+      console.log("args in Game ", args); // { id: '2' }
+      return games.find((game) => game.id == id);
+    },
+
+    author: (parents, args, context) => {
+      const { id } = args;
+      return authors.find((author) => author.id == id);
+    },
+    review: (parents, args, context) => {
+      const { id } = args;
+      return reviews.find((review) => review.id == id);
+    },
+  },
+
+  Game: {
+    reviews(parents, args, context) {
+      console.log("parent in Game ", parents); // { id: '2', title: 'God of War', author_id: '2' }
+      console.log("args in Game ", args); // {}
+      return reviews.filter((review) => review.game_id == parents.id);
+    },
+  },
+
+  Author: {
+    reviews(parents, args, context) {
+      console.log("parent in Author ", parents);
+      console.log("args in Author ", args);
+      return reviews.filter((review) => review.author_id == parents.id);
+    },
+  },
+
+  Review: {
+    author(parents, args, context) {
+      console.log("parent in Review ", parents);
+      console.log("args in Review ", args);
+      return authors.find((author) => author.id == parents.author_id);
+    },
+    game(parents, args, context) {
+      console.log("parent in Review ", parents);
+      console.log("args in Review ", args);
+      return games.find((game) => game.id == parents.game_id);
+    },
   },
 };
 
--- shema.js
+++ shema.js
@@ -5,25 +5,32 @@ type Game {
     id: ID!, 
     title: String!
     platform: [String!]
+    reviews: [Review!]
 }
 
 type Review {
     id: ID!,
     rating: Int!,
     content: String!
+    game: Game!
+    author: Author!
 }
 
 type Author {
     id: ID!,
     name: String!,
     verified: Boolean!
+    reviews: [Review!]
 }
 
 type Query { 
     reviews: [Review]
     games: [Game]
     authors: [Author]
+    review(id:ID!): Review
+    game(id: ID!): Game
+    author(id: ID!): Author
 }
 `;
 
-// the Quay type is the entry point for all read operations in a GraphQL API. ITS NOT OPTIONAL
+// the Query type is the entry point for all read operations in a GraphQL API. ITS NOT OPTIONAL
